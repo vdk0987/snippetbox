c1e7ba504629303bdc8f32c494c610e098de0a8c
vdk0987 <vdk0987@gmail.com>
2026-07-24 12:50:36 +0530

    create base and home page html templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,13 +10,20 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	ts, err := template.ParseFiles(".ui/html/pages/home.tmpl")
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal server errror", http.StatusInternalServerError)
 		return
 	}
-	err = ts.Execute(w, nil)
+
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
