ca76a19d2432eacf49a07dd26b0b13a980c7e07c
vdk0987 <vdk0987@gmail.com>
2026-07-31 20:37:33 +0530

    add helper for handling server errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,15 +19,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal server errror", http.StatusInternalServerError)
+		app.ServerError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.ServerError(w, r, err)
 	}
 }
 
