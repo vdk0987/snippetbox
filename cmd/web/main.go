9ca93d9a2be03744204245d676731cb5ecedf385
vdk0987 <vdk0987@gmail.com>
2026-08-22 21:49:32 +0530

    establish database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,9 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,9 +16,18 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "root:root@tcp(localhost:3306)/snippetbox?parseTime=true", "MySQl datasource name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(0)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
@@ -23,7 +35,22 @@ func main() {
 
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
