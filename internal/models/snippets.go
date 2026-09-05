b1d7403bce59537952c29380ef03c6c97791025d
vdk0987 <vdk0987@gmail.com>
2026-09-05 12:47:12 +0530

    function to get snippet from db

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -32,5 +33,23 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 	}
 
 	return int(id), nil
+}
 
+func (m *SnippetModel) Get(id int) (Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() and id=?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	var s Snippet
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
+		}
+	}
+	return s, nil
 }
